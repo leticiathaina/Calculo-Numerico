debbd4dd0ab1fc9b5a0f9e3317e4cfd1dda7d8b3
Leticia Thaina <106406586+leticiathaina@users.noreply.github.com>
2023-07-13 14:13:25 -0300

    .

---
+++ julia.jl
@@ -0,0 +1,11 @@
+function hello(n)
+
+for i=1:10
+    println("Olá mundo, i= $(i)")
+    println("O valor de e^i = $(exp(i))")
+    for j=i+1:10
+        println("O valor de log(j) para j = $(j) é $(log(j))")
+    end
+    for i+ -4.3:10
+        println("Ovalor de i = $(i)")
+end
